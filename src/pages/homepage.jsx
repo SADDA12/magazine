b5b929946d354807ff7af3523a5748d34032afa0
Samantha Adda <sammiedaga@gmail.com>
2024-05-15 14:30:00 +0000

    updates

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -201,19 +201,126 @@ export default function HomePage() {
               <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia tempora numquam!</p>
             </div>
           </div>
-
-
           </div> 
 
 
+          <div className="bg-white p-2 my-4">
+          <div className="bg-indigo-950 px-2 py-1"><p className="font-semibold text-white">Relevant Stories</p></div>
 
 
+          <div className="my-4 flex gap-2">
+            <div className="px-2 h-44 pt-32 bg-cover bg-center w-5/12" style={{
+              backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${newsimg})`,
+            }}>
+              <button className="bg-rose-500 py-1 px-4 text-white">Lifestyle</button>
+            </div>
+            <div className="w-7/12">
+              <p className="font-semibold">A Discount Toner Cartridge Is Better Than Ever.</p>
+              <p className="text-xs text-gray-500"><i class="fa-regular fa-user"></i> Mark Wiens <i class="fa-regular fa-calendar-days"></i> 03,April,2018 <i class="fa-regular fa-comment"></i> 06 Comments</p>
+              <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia tempora numquam!</p>
+            </div>
+          </div>
+
+
+          <div className="my-4 flex gap-2">
+            <div className="px-2 h-44 pt-32 bg-cover bg-center w-5/12" style={{
+              backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${newsimg})`,
+            }}>
+              <button className="bg-rose-500 py-1 px-4 text-white">Lifestyle</button>
+            </div>
+            <div className="w-7/12">
+              <p className="font-semibold">A Discount Toner Cartridge Is Better Than Ever.</p>
+              <p className="text-xs text-gray-500"><i class="fa-regular fa-user"></i> Mark Wiens <i class="fa-regular fa-calendar-days"></i> 03,April,2018 <i class="fa-regular fa-comment"></i> 06 Comments</p>
+              <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia tempora numquam!</p>
+            </div>
+          </div>
+
+
+          <div className="my-4 flex gap-2">
+            <div className="px-2 h-44 pt-32 bg-cover bg-center w-5/12" style={{
+              backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${newsimg})`,
+            }}>
+              <button className="bg-rose-500 py-1 px-4 text-white">Lifestyle</button>
+            </div>
+            <div className="w-7/12">
+              <p className="font-semibold">A Discount Toner Cartridge Is Better Than Ever.</p>
+              <p className="text-xs text-gray-500"><i class="fa-regular fa-user"></i> Mark Wiens <i class="fa-regular fa-calendar-days"></i> 03,April,2018 <i class="fa-regular fa-comment"></i> 06 Comments</p>
+              <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia tempora numquam!</p>
+            </div>
+          </div>
+          </div> 
 
 
 
           </div>
 
-         <div id="right div"></div>
+         <div id="right div">
+          <div className="bg-white p-2 my-4">
+            <div className="bg-indigo-950 px-2 py-1"><p className="font-semibold text-white">Editor's Pick</p></div>
+          
+
+          <div style={{
+            backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${newsimg})`,
+          }} className="h-44 bg-cover bg-center my-4 px-2 pt-32">
+            <button className="bg-rose-500 py-1 px-4 text-white">Travel</button>
+          </div>
+
+          <div>
+            <p className="font-semibold">A Discount Toner Cartridge Is Better Than Ever.</p>
+            <p className="text-xs text-gray-500"><i class="fa-regular fa-user"></i> Mark Wiens <i class="fa-regular fa-calendar-days"></i> 03,April,2018 <i class="fa-regular fa-comment"></i> 06 Comments</p>
+            <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore officia tempora numquam!</p>
+          </div>
+
+          <div className="border-t-2 py-4 my-4 flex gap-2">
+            <div className="w-5/12"><img src={newsimg} alt="posts"/></div>
+            <div className="w-7/12">
+              <p className="my-1">Help Finding Information Online is so easy</p>
+              <p className="text-xs text-gray-500"><i class="fa-regular fa-user"></i> Mark Wiens <i class="fa-regular fa-calendar-days"></i> 03,April,2018 <i class="fa-regular fa-comment"></i> 06 Comments</p>
+            </div>
+          </div>
+
+
+
+          <div className="border-t-2 py-4 my-4 flex gap-2">
+            <div className="w-5/12"><img src={newsimg} alt="posts"/></div>
+            <div className="w-7/12">
+              <p className="my-1">Help Finding Information Online is so easy</p>
+              <p className="text-xs text-gray-500"><i class="fa-regular fa-user"></i> Mark Wiens <i class="fa-regular fa-calendar-days"></i> 03,April,2018 <i class="fa-regular fa-comment"></i> 06 Comments</p>
+            </div>
+          </div>
+
+          <div className="border-t-2 py-4 my-4 flex gap-2">
+            <div className="w-5/12"><img src={newsimg} alt="posts"/></div>
+            <div className="w-7/12">
+              <p className="my-1">Help Finding Information Online is so easy</p>
+              <p className="text-xs text-gray-500"><i class="fa-regular fa-user"></i> Mark Wiens <i class="fa-regular fa-calendar-days"></i> 03,April,2018 <i class="fa-regular fa-comment"></i> 06 Comments</p>
+            </div>
+          </div>
+
+          <div className="bg-indigo-950 px-2 py-1"><p className="text-white font-semibold">Newsletter</p></div>
+
+          <p className="text-gray-500 my-2">Here, I focus on a range of items and features that we use in life without giving them a second thought</p>
+
+          <div className="my-4">
+            <input type="text" placeholder="Enter Email" className="p-1 bg-gray-100"></input>
+            <button className="bg-rose-500 py-1 px-4 text-white">Subscribe</button>
+          </div>
+
+          <p className="text-gray-500">You can unsubscribe us at any time</p>
+
+          <div className="bg-black text-white p-2 my-4">Social Networks</div>
+
+          <div className="bg-blue-800 text-white p-2 my-4"><i class="fa-brands fa-facebook-f mx-2 border-r pr-2"></i><span className="px-4 border-r">983 Likes</span><a href="/" className="pl-4">Like our page</a></div>
+
+          <div className="bg-blue-200 text-white p-2 my-4"><i class="fa-brands fa-twitter mx-2 border-r pr-2"></i><span className="px-4 border-r">983 Followers</span><a href="/" className="pl-4">Like our page</a></div>
+
+          <div className="bg-red-700 text-white p-2 my-4"><i class="fa-brands fa-youtube mx-2 border-r pr-2"></i><span className="px-4 border-r">983 Subscribers</span><a href="/" className="pl-4">Like our page</a></div>
+
+          <div className="bg-orange-500 text-white p-2 my-4"><i class="fa-solid fa-wifi mx-2 border-r pr-2"></i><span className="px-4 border-r">983 Subscribe</span><a href="/" className="pl-4">Like our page</a></div>
+
+
+          </div>
+         </div>
 
         </section>
 
